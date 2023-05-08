045db07fc74ff30bc3e8776fddb49777e9cb24fb
federicofuria <federicofuriadev@gmail.com>
2023-05-08 19:33:48 -0300

    cargando proyecto 'mercado liebre'

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+
+app.listen(3000, () => {
+    console.log('Escuchando puerto 3000');
+});
